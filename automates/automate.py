52d49d6ffb17ef339115e13a8d5c4bab56b84ac9
Dereck DANIEL <daniel.dereck@gmail.com>
2021-04-28 11:42:10 +0200

    create automate class

---
+++ automates/automate.py
@@ -0,0 +1,17 @@
+class Automate:
+    unit_no
+    automate_no
+    type_automate
+    tank_temperature
+    ext_temperature
+    milk_weight_tank
+    final_weight
+    ph
+    k_plus
+    nacl_concentration
+    salmonella_bacteria_level
+    e_coli_bacteria_level
+    listeria_bacteria_level
+
+    def JSON_to_class (self):
+
